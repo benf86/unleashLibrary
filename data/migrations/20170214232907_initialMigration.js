73aa02b12c35a8ad4e925a455a7b5deff9d6ee5d
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-02-15 00:43:20 +0100

    add initial db migration

---
+++ data/migrations/20170214232907_initialMigration.js
@@ -0,0 +1,19 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.createTableIfNotExists('contents', function (table) {
+    table.increments('id');
+    table.integer('id_author');
+    table.string('link');
+    table.boolean('public');
+    table.boolean('published');
+    table.integer('upvotes');
+    table.timestamps();
+    table.integer('created_by');
+    table.string('id_unleash_path');
+    table.string('id_unleash_node');
+    table.string('type');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('contents');
+};
