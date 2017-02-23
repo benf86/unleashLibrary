8cfa1ce7711ea52a83a6a550c3bf578a18820eb3
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-02-23 17:35:26 +0100

    add service name to services object

--- services/base.js
+++ services/base.js
@@ -1,4 +1,5 @@
 module.exports = globals => serviceName => ({
+  service: serviceName,
   get: params => query => body => globals.repositories[serviceName].get(params)(body),
   create: params => query => body => globals.repositories[serviceName].create(params)(body),
   update: params => query => body => globals.repositories[serviceName].update(params)(body),
