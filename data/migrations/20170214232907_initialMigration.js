321ed0ddfb1e0bb1d09d69bc2ee8b62bf32491b0
Rolando Benjamin Vaz Ferreira <contact@ferreira.si>
2017-02-28 02:48:55 +0100

    update migrations

--- data/migrations/20170214232907_initialMigration.js
+++ data/migrations/20170214232907_initialMigration.js
@@ -1,11 +1,15 @@
+const contentStages = require('../../models/content').stages;
+
 exports.up = function (knex, Promise) {
   return knex.schema.createTableIfNotExists('contents', function (table) {
     table.increments('id');
-    table.integer('id_author');
+    table.string('id_author');
     table.string('link');
+    table.string('title');
     table.boolean('public');
     table.boolean('published');
     table.integer('upvotes');
+    table.enu('stage', contentStages);
     table.timestamps();
     table.integer('created_by');
     table.string('id_unleash_path');
